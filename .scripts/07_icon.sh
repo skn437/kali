3ebc602947db638e41e742466c0162af4c7f41c0
skn437 <skn437physx@gmail.com>
2024-05-28 13:41:10 +0600

    Completed Writing Icon Script

--- .scripts/07_icon.sh
+++ .scripts/07_icon.sh
@@ -8,4 +8,6 @@ if test -d "${ICON_DIRECTORY}"; then
   rm -rf "${ICON_DIRECTORY}"
 fi
 
-curl -sSL "" | tar -xzf >"${ICON_DIRECTORY}"
+mkdir -p "${ICON_DIRECTORY}"
+
+curl -sSL https://raw.githubusercontent.com/skn437/ubuntu/master/.idea/icon.tar.gz | tar -xz -C "${ICON_DIRECTORY}"
