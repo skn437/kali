565da91555ce106d1784bc83c429d4ecab966163
SKN Shukhan <skn437physx@gmail.com>
2024-05-29 03:14:31 +0600

    Updated 01_initial.sh

--- .scripts/01_initial.sh
+++ .scripts/01_initial.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-apt_prepare() {
-  sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-}
+sudo systemctl enable bluetooth.service
 
-echo 'export PROMPT_COMMAND="printf '\n';$PROMPT_COMMAND"' >>$HOME/.bashrc
-
-apt_prepare && sudo apt install -y curl build-essential git gnome-tweaks
+printf "Bluetooth Enabled ✅ \n"
