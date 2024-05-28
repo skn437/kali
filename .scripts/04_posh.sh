c3537ae7e77719dd77c3f23c4f793d59de4c2a06
skn437 <skn437physx@gmail.com>
2024-05-28 14:18:21 +0600

    Added Oh My Posh Script

---
+++ .scripts/04_posh.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+set -e
+
+package_install() {
+  printf "'%s' Installed! ✅ \n" "$1"
+}
+
+oh_my_posh() {
+  # Check if environment variable `$TERM` is set to `xterm-256color` or not
+  printf "TERM = %s \n" "$(echo $TERM)"
+
+  # Install "Oh My Posh"
+  brew install oh-my-posh
+}
+
+oh_my_posh_init() {
+  # Install "FiraCode font"
+  oh-my-posh font install
+
+  # Get your current shell
+  printf "SHELL = %s \n" "$(oh-my-posh get shell)"
+
+  # Install theme
+  echo 'eval "$(oh-my-posh init bash --config $(brew --prefix oh-my-posh)/themes/kali.omp.json)"' >>~/.bashrc
+}
+
+oh_my_posh
+
+printf "\n"
+
+oh_my_posh_init
+
+package_install "Oh My Posh"
