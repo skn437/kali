285af229d16159de1b8d954bbd4ca8ea6b10a76e
SKN Shukhan <skn437physx@gmail.com>
2024-05-29 03:26:22 +0600

    Updated 06_service.sh

--- .scripts/06_service.sh
+++ .scripts/06_service.sh
@@ -7,8 +7,16 @@ package_install() {
 }
 
 docker() {
-  # Docker
-  curl -sSL https://get.docker.com | bash
+  # Set up the `APT` repository
+  echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list
+
+  # Import the `GPG` key
+  curl -fsSL https://download.docker.com/linux/debian/gpg |
+  sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+  # Download "docker-ce-cli", "containerd.io", "docker-ce", "docker-buildx-plugin", "docker-compose-plugin" packages
+  sudo apt install -y docker-ce-cli containerd.io docker-ce docker-buildx-plugin docker-compose-plugin
 
   # Create a `Docker Group` and add linux username to it
   sudo usermod -aG docker $USER && newgrp docker
