8a561865bfbd236bba5218ccf54e8b6e3cbb47c4
SKN Shukhan <skn437physx@gmail.com>
2024-05-30 02:07:20 +0600

    Updated 01_initial.sh for Ubuntu

--- .scripts/01_initial.sh
+++ .scripts/01_initial.sh
@@ -2,6 +2,12 @@
 
 set -e
 
-sudo systemctl enable bluetooth.service
+apt_prepare {
+  sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+}
 
-printf "Bluetooth Enabled ✅ \n"
+main_package() {
+  sudo apt install -y curl build-essential git gnome-tweaks
+}
+
+apt_prepare && main_package && printf "Main Packages Installed! ✅ \n"
