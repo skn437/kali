ffc108c1daa7ef1b47b93bab33686e5acf559008
SKN Shukhan <skn437physx@gmail.com>
2024-05-30 02:31:05 +0600

    Updated 08_icon.sh yet again

--- .scripts/08_icon.sh
+++ .scripts/08_icon.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-icon_install() {
-  printf "'%s' Installed! ✅ \n" "$1"
-}
-
 ICON_DIRECTORY="$HOME/.intellij-idea"
 
 if test -d "${ICON_DIRECTORY}"; then
@@ -16,4 +12,4 @@ mkdir -p "${ICON_DIRECTORY}"
 
 curl -sSL https://raw.githubusercontent.com/skn437/ubuntu/master/.idea/icon.tar.gz | tar -xz -C "${ICON_DIRECTORY}"
 
-icon_install "Icons"
+printf "'Icon' Installed! ✅ \n"
