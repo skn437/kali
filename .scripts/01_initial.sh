6a0c7bd9350b5cb4907adf6fcc610d6a10fb70ae
SKN Shukhan <skn437physx@gmail.com>
2024-05-30 20:38:03 +0600

    Updated 01_initial.sh for Kali again

--- .scripts/01_initial.sh
+++ .scripts/01_initial.sh
@@ -2,26 +2,6 @@
 
 set -e
 
-apt_prepare() {
-  sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-}
+sudo systemctl enable bluetooth.service
 
-main_package() {
-  sudo apt install -y curl build-essential git gnome-tweaks zsh
-}
-
-zsh() {
-  # Get `zsh` version
-  printf "zsh version: %s \n" "$(zsh --version)"
-
-  # Initialize `zsh`
-  zsh
-
-  # Make `zsh` as default shell
-  chsh -s "$(which zsh)"
-
-  # Check current active shell
-  printf "Current shell: %s \n" $SHELL
-}
-
-apt_prepare && main_package && zsh && printf "Main Packages Installed! ✅ \n"
+printf "Bluetooth Enabled! ✅ \n"
