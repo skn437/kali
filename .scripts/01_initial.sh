13d9d83014c1cc9bfe69e28832f047bde288a835
SKN Shukhan <skn437physx@gmail.com>
2024-05-31 03:31:54 +0600

    Updated 01_initial.sh for Ubuntu

--- .scripts/01_initial.sh
+++ .scripts/01_initial.sh
@@ -2,6 +2,26 @@
 
 set -e
 
-sudo systemctl enable bluetooth.service
+message() {
+  printf "%s! ✅ \n" "$1" 
+}
 
-printf "Bluetooth Enabled! ✅ \n"
+apt_prepare() {
+  sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+}
+
+main_package() {
+  # `curl`, `git`, `build-essential`, `gnome-tweaks` & `zsh`
+  sudo apt install -y curl git build-essential gnome-tweaks zsh
+  
+  message "'Main Packages' Installed"
+}
+
+zsh() {
+  # Change default shell to `zsh` 
+  chsh -s "$(which zsh)"
+
+  message "'Zsh' Activated!"
+}
+
+apt_prepare && main_package && zsh
