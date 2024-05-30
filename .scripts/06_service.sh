0f20de5820febc155610cecea8c0d7217f896442
SKN Shukhan <skn437physx@gmail.com>
2024-05-31 03:49:27 +0600

    Update 06_service.sh

--- .scripts/06_service.sh
+++ .scripts/06_service.sh
@@ -10,32 +10,22 @@ brew_prepare() {
   brew update && brew upgrade && brew autoremove && brew cleanup
 }
 
-package_install() {
-  printf "'%s' Installed! ✅ \n" "$1"
-}
-
-docker_key() {
-  # Set up the `apt` repository
-  echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list
-
-  # Import the `gpg` key
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-  package_install "Docker Key"
+message() {
+  printf "%s! ✅ \n" "$1"
 }
 
 docker() {
   # Download "docker-ce-cli", "containerd.io", "docker-ce", "docker-buildx-plugin", "docker-compose-plugin" packages
-  sudo apt install -y docker-ce-cli containerd.io docker-ce docker-buildx-plugin docker-compose-plugin
+  curl -sSL https://get.docker.com | bash
 
-  package_install "Docker"
+  message "Docker Installed"
 }
 
 docker_init() {
   # Create a `Docker Group` and add linux username to it
   sudo usermod -aG docker $USER && newgrp docker
 
-  package_install "Docker Group"
+  message "Docker Group Enabled"
 }
 
 kubernetes() {
@@ -48,7 +38,7 @@ kubernetes() {
   # Ingress
   minikube addons enable ingress
 
-  package_install "Kubernetes"
+  message "Kubernetes Installed"
 }
 
-docker_key && apt_prepare && docker && docker_init && brew_prepare && kubernetes
+apt_prepare && docker && docker_init && brew_prepare && kubernetes
