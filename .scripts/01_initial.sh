7f3bb513f50d830864c48adfe979f22ee07f3fa0
SKN Shukhan <skn437physx@gmail.com>
2024-06-01 03:33:14 +0600

    Update 01_initial.sh

--- .scripts/01_initial.sh
+++ .scripts/01_initial.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-message() {
-  printf "%s! ✅ \n" "$1" 
-}
-
 apt_prepare() {
   sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 }
@@ -14,14 +10,12 @@ main_package() {
   # `curl`, `git`, `build-essential`, `gnome-tweaks` & `zsh`
   sudo apt install -y curl git build-essential gnome-tweaks zsh
   
-  message "'Main Packages' Installed"
+  printf "'Main Packages' Installed! ✅ \n"
 }
 
 zsh_init() {
   # Initialize `zsh`
-  gnome-terminal -- bash -c "printf 'zsh version: %s \n' '$(zsh --version)'; zsh; chsh -s '$(which zsh)'"
-
-  message "'Zsh' Initialized"
+  gnome-terminal -- bash -c "printf 'zsh version: %s \n' '$(zsh --version)'; zsh; chsh -s '$(which zsh)'; printf 'Zsh Initialized! ✅ \n'"
 }
 
 apt_prepare && main_package && zsh_init
