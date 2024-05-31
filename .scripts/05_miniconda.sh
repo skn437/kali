bf4ccb17d2f08986224a2ba53054ec0aeaacc00a
SKN Shukhan <skn437physx@gmail.com>
2024-05-31 22:33:06 +0600

    Update 05_miniconda.sh

--- .scripts/05_miniconda.sh
+++ .scripts/05_miniconda.sh
@@ -3,6 +3,7 @@
 set -e
 
 miniconda() {
+  # Miniconda
   mkdir -p $HOME/miniconda3
   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda3/miniconda.sh
   bash $HOME/miniconda3/miniconda.sh -b -u -p $HOME/miniconda3
@@ -10,6 +11,7 @@ miniconda() {
 }
 
 miniconda_init() {
+  # Initialize `Miniconda` to both `.zshrc` & `.bashrc`
   $HOME/miniconda3/bin/conda init zsh
   $HOME/miniconda3/bin/conda init bash
 }
