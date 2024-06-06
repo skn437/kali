d8122f1db73ea6cb8dafc0aef0cecf3aeff826ff
SKN Shukhan <skn437physx@gmail.com>
2024-06-06 17:18:46 +0600

    Update 01_initial.sh

--- .scripts/01_initial.sh
+++ .scripts/01_initial.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-apt_prepare() {
-  sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-}
-
 bluetooth() {
   sudo systemctl enable bluetooth.service
   printf "'Bluetooth' Initialized! ✅ \n"
@@ -15,4 +11,4 @@ reboot() {
   gnome-terminal -- bash -c "printf 'The system needs a reboot to activate the changes! 📚'; printf 'Preparing to reboot... \n'; sleep 2; sudo reboot now"
 }
 
-apt_prepare && bluetooth && reboot
+bluetooth && reboot
