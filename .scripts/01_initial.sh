def98b240d3acb0dc051642446a46981eff45faa
skn437 <skn437physx@gmail.com>
2024-06-08 20:11:35 +0600

    Updated initial script

--- .scripts/01_initial.sh
+++ .scripts/01_initial.sh
@@ -15,14 +15,25 @@ bluetooth() {
 jetbrains() {
   # Intellij Idea Ultimate
   echo 'export PATH="$HOME/.intellij-idea/bin:$PATH"' >>$HOME/.zshrc
+
   # Rust Rover
   echo 'export PATH="$HOME/.rust-rover/bin:$PATH"' >>$HOME/.zshrc
 
   message "'JetBrains IDE' Configured In Path"
 }
 
+desktop() {
+  # Postman
+  echo 'export PATH="$HOME/.postman-desktop/app:$PATH"' >>$HOME/.zshrc
+
+  # Neo4j
+  echo 'export PATH="$HOME/.neo4j-desktop:$PATH"' >>$HOME/.zshrc
+
+  message "'Desktop App' Configured In Path"
+}
+
 reboot() {
   gnome-terminal -- bash -c "printf 'The system needs a reboot to activate the changes! 📚'; printf 'Preparing to reboot... \n'; sleep 2; sudo reboot now"
 }
 
-bluetooth && jetbrains && reboot
+bluetooth && jetbrains && desktop && reboot
