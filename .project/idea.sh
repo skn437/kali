a5dc24e14b3aacd05d200e9d887a85e7d576b5b0
skn437 <skn437physx@gmail.com>
2024-07-26 17:38:26 +0600

    Updated Git Ignore & Icon For Intellij Idea

--- .project/idea.sh
+++ .project/idea.sh
@@ -5,13 +5,17 @@ set -e
 LINUX="kali"
 
 idea() {
+  IDEA_DIRECTORY="./.idea"
   JSON_SCHEMA="jsonSchemas.xml"
 
-  mkdir -p ./.idea
+  if test -d "${IDEA_DIRECTORY}"; then
+    curl -sSL "https://raw.githubusercontent.com/skn437/${LINUX}/master/.project/helpers/${JSON_SCHEMA}" >"${IDEA_DIRECTORY}/${JSON_SCHEMA}"
 
-  curl -sSL "https://raw.githubusercontent.com/skn437/${LINUX}/master/.idea/${JSON_SCHEMA}" >"./.idea/${JSON_SCHEMA}"
-
-  printf "'Intellij Idea JSON Schema' Added! ✅ \n"
+    printf "'Intellij Idea JSON Schema' Added! ✅ \n"
+  else
+    printf "'.idea' Directory Absent! ❌ \n"
+    exit 1
+  fi
 }
 
 idea
