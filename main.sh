81740107a91c4b8585b94e93a475655cb5b4adde
skn437 <skn437physx@gmail.com>
2026-02-12 13:52:11 +0600

    Added Vercel Icons

--- main.sh
+++ main.sh
@@ -2,11 +2,5 @@
 
 set -e
 
-SHELL_LIBRARY_FILE="$HOME/.shell-lib/ssu/main.sh"
-
-if ! test -f "${SHELL_LIBRARY_FILE}"; then
-  curl -sSL https://raw.githubusercontent.com/skn437/skn-shell-utils/master/build.sh | sh
-fi
-
-# shellcheck disable=SC1090
-source "${SHELL_LIBRARY_FILE}"
+# shellcheck disable=SC1091
+source "$HOME/.shell-lib/ssu/main.sh"
