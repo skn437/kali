4f40b237450ef4553d34d51d82942ba800e19b02
skn437 <skn437physx@gmail.com>
2026-04-01 02:45:09 +0600

    Added so many astro related icons again

---
+++ .project/npmrc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -e
+
+LINUX="kali"
+
+npmrc() {
+  NPM_CONFIG=".npmrc"
+
+  if test -f "package.json"; then
+    curl -sSL "https://raw.githubusercontent.com/skn437/${LINUX}/master/${NPM_CONFIG}" >"./${NPM_CONFIG}"
+
+    printf "'NPM RC' Added! ✅ \n"
+  else
+    printf "'package.json' File Absent! ❌ \n"
+
+    exit 1
+  fi
+}
+
+npmrc
